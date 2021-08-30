b27b5cb0418004dc64beb5261527d4f431fc6fd7
FastnBulbous81 <m.williams.entropy@gmail.com>
2021-08-30 21:13:36 +0100

    Moved header content to header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,14 @@
 import React from 'react'
 
-function Header({ children }) {
+function Header() {
   return (
     <div className="bg-primary bg-opacity-95">
       <div className="container">
-        <div className="flex justify-center text-light text-center px-3 py-12 my-6">{children}</div>
+        <div className="flex justify-center text-light text-center px-3 py-12 my-6">
+          <div className="text-4xl">
+            <h1>Mark Williams the Big Geek</h1>
+          </div>
+        </div>
       </div>
     </div>
   )
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -5,11 +5,7 @@ import Header from './Header'
 function Layout({ children }) {
   return (
     <>
-      <Header>
-        <div className="text-4xl">
-          <h1>Mark Williams the Big Geek</h1>
-        </div>
-      </Header>
+      <Header />
       {children}
     </>
   )
