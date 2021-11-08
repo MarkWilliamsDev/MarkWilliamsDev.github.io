5e3cb5c65e4777c942b68cb27a868093d098d5a1
FastnBulbous81 <m.williams.entropy@gmail.com>
2021-11-08 20:44:41 +0000

    Moved ToggleList controls to a lower level

--- src/components/ToggleList.js
+++ src/components/ToggleList.js
@@ -16,27 +16,25 @@ const ToggleList = ({ ListTitle, list }) => {
 
   return (
     <>
-      <div
-        className="flex justify-center pt-2"
-        onClick={handleOnClick}
-        onKeyDown={handleOnClick}
-        aria-expanded={showList}
-        role="button"
-        tabIndex={0}
-      >
-        <div>
-          <div className="relative text-xl font-sans">
-            <div className="absolute -inset-x-8 -inset-y-1 flex">
-              <div
-                className={`toggle-arrow ${
-                  showList ? 'toggled-toggle-arrow' : ''
-                } transform hover:scale-110`}
-              >
-                {renderToggleArrow()}
-              </div>
+      <div className="flex justify-center pt-2">
+        <div
+          className="relative text-xl font-sans"
+          onClick={handleOnClick}
+          onKeyDown={handleOnClick}
+          aria-expanded={showList}
+          role="button"
+          tabIndex={0}
+        >
+          <div className="absolute -inset-x-8 -inset-y-1 flex">
+            <div
+              className={`toggle-arrow ${
+                showList ? 'toggled-toggle-arrow' : ''
+              } transform hover:scale-110`}
+            >
+              {renderToggleArrow()}
             </div>
-            <div className="select-none text-xl font-arvo">{ListTitle}</div>
           </div>
+          <div className="select-none text-xl font-arvo">{ListTitle}</div>
         </div>
       </div>
       <div className="flex justify-center md:pl-20">{showList ? <List list={list} /> : ''}</div>
